40af4282f5893d6c5736958e850e638fcf7e316c
jiyolla <jiyolla0919@gmail.com>
2021-08-10 19:53:26 +0900

    Add initial api endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,6 +21,21 @@ def get_db():
     finally:
         db.close()
 
+@app.get('/api/champions')
+def get_champions():
+    ...
+
+@app.get('/api/champions/{champion_id}/skins')
+def get_champion_skins():
+    ...
+
+@app.get('/api/recommendations')
+def get_recommendations():
+    ...
+
+@app.get('/api/skins/{skin_id}')
+def get_skin():
+    ...
 
 @app.get("/champion/{champion_name}/skins/", response_model=List[schemas.Skin])
 def get_skins_by_champion_name(champion_name: str, db: Session = Depends(get_db)):
