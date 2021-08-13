884ceeaea50901d8b3aa95a33f074fa492b38be3
Remian103 <alsdnska@naver.com>
2021-08-13 10:48:24 +0900

    Header 추가
    anchor를 통해 페이지 네비게이션 기능 추가
    sticky 속성을 통해 화면 상단에 따라다니는 헤더 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,57 @@
 import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './NavAnchor';
 import {
-  Div,
-  Button,
-  Text,
-  Icon,
-  Anchor
+    Div,
+    Button,
+    Text,
+    Icon
 } from "atomize";
 
 function App() {
 
-  //console test log
-  useEffect(() => {
-    console.log("test log");
-  }, [])
+    //console test log
+    useEffect(() => {
+        console.log("test log");
+    }, [])
 
-  return (
-    <div>
-      <header>
-        <nav style={{display: "flex"}}>
-          <Anchor href="#recommend-skin">
-            <Button
-                bg="info700"
-                hoverBg="info600"
-                m={{ x: "1rem", y: "1rem" }}
-                cursor="pointer"
-                rounded="md"
-            >
-              추천 스킨
-            </Button>
-          </Anchor>
-          <Anchor href="#champ">
-            <Button
-                bg="info700"
-                hoverBg="info600"
-                m={{ x: "1rem", y: "1rem" }}
-                cursor="pointer"
-                rounded="md"
-            >
-              챔피언
-            </Button>
-          </Anchor>
-        </nav>
-      </header>
-      <div id="recommend-skin" className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </div>
-    </div>
-  );
+    const anchorList = [
+        { name: "추천 스킨", link: "#recommend-skins" },
+        { name: "챔피언 목록", link: "#champions" }
+    ];
+
+    return (
+        <>
+            <Div className="main-background" />
+            <header className="main-header">
+                <Nav anchorList={anchorList} />
+            </header>
+            <body>
+                <Div className="container">
+                    <Div className="item">1</Div>
+                    <Div className="item">2</Div>
+                    <Div className="item">3</Div>
+                    <Div className="item">4</Div>
+                    <Div className="item">5</Div>
+                </Div>
+                <div id="recommend-skins" className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </div>
+            </body>
+        </>
+    );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import { ThemeProvider, StyleReset } from 'atomize';
 import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
