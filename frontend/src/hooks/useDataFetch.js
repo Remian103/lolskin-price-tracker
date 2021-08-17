98cb27022d7c7476c695700252e40d8477b0c1ed
Remian103 <alsdnska@naver.com>
2021-08-17 22:54:25 +0900

    implement custom hook for data fetching
    make new folder ./src/hooks for custom hooks
    now, we can use "useDataFetch" for data fetching from url

---
+++ frontend/src/hooks/useDataFetch.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useReducer } from "react";
+import axios from "axios";
+
+function dataFetchReducer(state, action) {
+    switch (action.type) {
+        case "FETCH_INIT":
+            return {
+                ...state,
+                isLoading: true,
+                isError: false
+            };
+        case "FETCH_SUCCESS":
+            return {
+                isLoading: false,
+                isError: false,
+                data: action.payload
+            };
+        case "FETCH_FAILURE":
+            return {
+                ...state,
+                isLoading: false,
+                isError: true
+            };
+        default:
+            throw new Error();
+    }
+}
+
+function useDataFetch({ initialUrl, initialData }) {
+    const [url, setUrl] = useState(initialUrl);
+    const [state, dispatch] = useReducer(dataFetchReducer, {
+        isLoading: false,
+        isError: false,
+        data: initalData
+    });
+
+    useEffect(() => {
+        let didCancel = false;
+
+        const fetchData = async () => {
+            dispatch({ type: "FETCH_INIT" });
+
+            try {
+                const result = await axios(url);
+                if (!didCancel)
+                    dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+            } catch (error) {
+                if (!didCancel)
+                    dispatch({ type: "FETCH_FAILURE" });
+            }
+        };
+
+        fetchData();
+
+        return () => {
+            didCancel = true;
+        };
+    }, [url]);
+
+    return [state, setUrl];
+}
