cd4eb4b61830395d104f1ed097ab0b8503969191
jiyolla <jiyolla0919@gmail.com>
2021-08-19 02:56:04 +0900

    Add script to update price history table
    And also a league-client-stuff folder is created.
    Some refactoring is expected after the feature is fully functional and
    bug free.

--- backend/app/update_price_history.py
+++ backend/app/update_price_history.py
@@ -9,13 +9,24 @@ from . import models
 
 connector = Connector()
 
+
 async def update(connection, db, skin):
-    skin_data = await connection.request('get', f'/lol-store/v1/skins/{skin.id}')
+    skin_api_uri = f'/lol-store/v1/skins/{skin.id}'
+    skin_data = await connection.request('get', skin_api_uri)
     skin_data = await skin_data.json()
-    if skin.id == 1:
-        print(skin_data)
-    price = skin_data['prices'][0]['cost']
-    sale_price = 0# skin_data['sale']
+
+    # If the item is available in store
+    try:
+        price = skin_data['prices'][0]['cost']
+    except Exception:
+        price = 0
+
+    # If the item is on sale
+    try:
+        sale_price = skin_data['sale']['prices'][0]['cost']
+    except Exception:
+        sale_price = 0
+
     db_price_history = models.Price_History(skin=skin, date=date.today(), price=price, sale_price=sale_price)
     db.add(db_price_history)
 
---
+++ backend/league-client-stuff/README.md
@@ -0,0 +1,15 @@
+## About
+A messy folder for files/instructions about deployment on the server where League Client and the update script runs.
+
+### How to reformulate current image?
+Install
+- [League Client](https://download.kr.riotgames.com/league?_gl=1*e509tb*_ga*NTYwNTAzODc0LjE2Mjg2NjI5NjE.*_ga_FXBJE5DEDD*MTYyOTMwNzk1NS40LjAuMTYyOTMwNzk1NS42MA..)
+- [Python](https://www.python.org/downloads/)
+- [Git](https://git-scm.com/downloads)
+- [Poetry](https://python-poetry.org/docs/)
+
+Configure
+- [Enable Windows Auto logon](https://docs.microsoft.com/en-us/sysinternals/downloads/autologon)
+- register 'update_script.ps1' as daily task
+
+...
