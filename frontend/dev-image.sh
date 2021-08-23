b6e676192d7d06c587683710eda9968a1d7fbe1b
Remian103 <alsdnska@naver.com>
2021-08-23 23:04:23 +0900

    Docker setting modified / readme update
    docker container bash is now open with user 'node', not root.
    readme update

--- frontend/dev-image.sh
+++ frontend/dev-image.sh
@@ -14,6 +14,8 @@ then
     echo "Replace package.json and yarn.lock"
     cp ./testPackage/package.json ./package.json
     cp ./testPackage/yarn.lock ./yarn.lock
+    rm ./testPackage/package.json
+    rm ./testPackage/yarn.lock
 fi
 
 # save previous image's id
@@ -22,13 +24,15 @@ echo $id
 
 # Build image
 echo "Build docker image  'remian103/frontend-dev'"
-docker build -f Dockerfile.dev -t remian103/frontend-dev .
+docker build -f Dockerfile.dev -t remian103/frontend-dev \
+    --build-arg USER_ID=$(id -u) \
+    --build-arg GROUP_ID=$(id -g) .
 
 # Delete previous image
 if [ -n ${id} ];
 then
-    name=`docker images | grep ${id} | awk '{print $1}'`
-    if [ ${name} != "remian103/frontend-dev" ];
+    name=`docker images | grep ${id} | awk '{print $2}'`
+    if [ ${name} != "latest" ];
     then
         echo "Delete old image.."
         docker rmi ${id}
