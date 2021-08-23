010703a6a71a9039e54bfb9439786456bdb4acaa
jiyolla <jiyolla0919@gmail.com>
2021-08-24 00:46:25 +0900

    Change 'db_config.ini' file search directory
    Now, 'db_config.ini' has to be located in backend directory regaredless
    of the current directory, which is good!
    
    Previously, if one run some script that load db_config in a folder other
    backend, 'db_config.ini' in the backend would not be loaded.
    
    For example, '/path/that/is/not/backend/$ python some_script.py' would
    failed to load 'db_config.ini' in the backend folder.
    
    But now, search for the 'db_config.ini' is relative to the location of
    'db_config.py'. 'db_config.py' will always search the config file in
    it's parent folder.

--- backend/app/db_config.py
+++ backend/app/db_config.py
@@ -1,12 +1,14 @@
+import os
 from configparser import ConfigParser
 
 
 # Try to load 'db_config.ini'.
 # Will use local SQlite on any kind of failure(e.g. 'db_config.ini' not exists, not properly written...)
 # Note that this does not guarantee that the DB_URL is actually reachable.
-try: 
+try:
     config = ConfigParser()
-    config.read('db_config.ini')
+    config_file = os.path.join(os.path.dirname(__file__), '../db_config.ini')
+    config.read(config_file)
     db_config = config[config['active']['profile']]
     DB_URL = f'{db_config["dialect"]}+{db_config["driver"]}://{db_config["username"]}:{db_config["password"]}@{db_config["host"]}:{db_config["port"]}/{db_config["database_name"]}'
     print(f'Using {db_config["prompt_name"]}...')
