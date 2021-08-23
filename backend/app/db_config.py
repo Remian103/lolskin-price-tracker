6a56920cf12846c6067d7b71366ff45d6b833935
jiyolla <jiyolla0919@gmail.com>
2021-08-24 02:10:23 +0900

    Change search location of local sqlite
    It is now fixed at 'backend' directory regardless of the current
    directory.

--- backend/app/db_config.py
+++ backend/app/db_config.py
@@ -6,6 +6,7 @@ from configparser import ConfigParser
 # Will use local SQlite on any kind of failure(e.g. 'db_config.ini' not exists, not properly written...)
 # Note that this does not guarantee that the DB_URL is actually reachable.
 try:
+    # raise()
     config = ConfigParser()
     config_file = os.path.join(os.path.dirname(__file__), '../db_config.ini')
     config.read(config_file)
@@ -13,5 +14,5 @@ try:
     DB_URL = f'{db_config["dialect"]}+{db_config["driver"]}://{db_config["username"]}:{db_config["password"]}@{db_config["host"]}:{db_config["port"]}/{db_config["database_name"]}'
     print(f'Using {db_config["prompt_name"]}...')
 except Exception:
-    DB_URL = 'sqlite:///./sqlite3.db'
+    DB_URL = f'sqlite:///{os.path.join(os.path.dirname(__file__), "../sqlite3.db")}'
     print(f'Using local SQLite...')
