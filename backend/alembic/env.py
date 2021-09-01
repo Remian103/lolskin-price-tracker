498219a925d0a0d894b0258b02c2fcb075751738
jiyolla <jiyolla0919@gmail.com>
2021-09-01 21:32:17 +0900

    Add dry-run feature for migration scripts
    See
    https://github.com/sqlalchemy/alembic/issues/829
    
    Dry run for migration scripts on production database for double check.

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -59,11 +59,14 @@ def run_migrations_online():
     connectable = engine
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, target_metadata=target_metadata,
+            transactional_ddl=True
         )
-
-        with context.begin_transaction():
+        with context.begin_transaction() as transaction:
             context.run_migrations()
+            if 'dry-run' in context.get_x_argument():
+                print('Dry-run succeeded; now rolling back transaction...')
+                transaction.rollback()
 
 
 if context.is_offline_mode():
