fa99b9ff481829acef52e61a72c4469b0c4be938
jiyolla <jiyolla0919@gmail.com>
2021-09-03 01:16:42 +0900

    Rework run script
    Completely reworked 'run.sh'.
    
    'run.sh' now support two arguments
    --prod: to launch app with production config
    --port: to set listening port with 8000 as default
    
    In production config, the script expects itself to be run with virtual
    environment off.
    It will start the app with nohup in the background, also logging the
    output(both stdout and stderr) with timestamp in a file named
    'uvicorn_[START_TIME].log'.
    The log file is appended on each run.
    Use log file name to easily locate the start of last run.
    
    Original 'logs' directory and 'log_conf.json' is no longer used.
    
    In non-production config, the script expects itself to be run with
    virtual environment on, which is often activated while developing.
    The output is not redirected or timestamped either.

--- backend/run.sh
+++ backend/run.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
-if [ -d logs ]; then
-    rm -rdf logs
+
+PORT=8000
+
+while test $# != 0; do
+    case "$1" in
+        --prod)
+            PROD_MODE=true
+            shift
+            ;;
+        --port)
+            PORT=$2
+            shift
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if [ "$PROD_MODE" = "true" ]; then
+    OLD_LOG=`ls | grep uvicorn_*.log`
+    NEW_LOG=`gawk 'BEGIN{ print strftime("uvicorn_%y%m%dT%H%M%S.log"); }'`
+    if [ "$OLD_LOG" = "" ]; then
+        echo "Creating '$NEW_LOG'..."
+    else
+        echo "Renaming '$OLD_LOG' to '$NEW_LOG'..."
+        mv uvicorn_*.log $NEW_LOG
+    fi
+    echo "Start Logging..."
+    echo "====================================================================================================" >> $NEW_LOG
+    nohup poetry run uvicorn app.main:app --reload-dir app --reload --port $PORT |& gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0; fflush(); }' >> $NEW_LOG
+else 
+    uvicorn app.main:app --reload-dir app --reload --port $PORT
 fi
-mkdir logs
-poetry run uvicorn app.main:app --reload-dir app --reload --log-config=log_conf.json --port ${1:-8000}
