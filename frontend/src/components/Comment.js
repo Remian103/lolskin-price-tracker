c3b25a61041ff00133e1b5a01e9a2490d59ac59a
Remian103 <alsdnska@naver.com>
2021-09-08 16:18:50 +0900

    add logic in Comment.js : modify comment

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { Button, Div, Icon } from "atomize";
+import { Button, Div, Icon, Input } from "atomize";
 import axios from "axios";
 import "../css/Comment.css";
 
@@ -18,7 +18,7 @@ function Comment({ comment, modifyRequest }) {
         if (!userInfo.isLogin) {
             alert("로그인 후에 이용하실 수 있습니다.");
             return;
-        } 
+        }
 
         setLoading(true);
 
@@ -38,6 +38,23 @@ function Comment({ comment, modifyRequest }) {
         setLoading(false);
     };
 
+    const [modifyMode, setMode] = useState(false);
+    const [content, setContent] = useState(comment.content);
+    const handleModifyBtn = (event) => {
+        event.preventDefault();
+        setContent(comment.content);
+        setMode(true);
+    };
+    const handleConentChange = (event) => {
+        setContent(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        modifyRequest(`api/comments/${comment.comment_id}`, { content: content });
+        setMode(false);
+    };
+
     return (
         <Div className="comment-animation shadowDiv"
             pos="relative"
@@ -47,25 +64,54 @@ function Comment({ comment, modifyRequest }) {
             minH="3rem"
             rounded="md"
         >
-            <Div
-                pos="absolute"
-                top="1rem"
-                right="1rem"
-            >
-                {likes}
-                <Icon onClick={handleClickLike} name={isLike ? "HeartSolid" : "Heart"} size="20px" color="info700" />
-            </Div>
-            <Div
-                p={{ r: "4rem" }}
-                style={{
-                    wordBreak: "break-all"
-                }}
-            >
-                {comment.content}
-            </Div>
-            <Button onClick={()=>{modifyRequest(`api/comments/${comment.comment_id}`, {content: "test"})}}>
-                수정
-            </Button>
+            {modifyMode ? <>
+                <form
+                    onSubmit={handleSubmit}
+                >
+                    <Input className="shadowDiv"
+                        type="text"
+                        value={content}
+                        onChange={handleConentChange}
+                        placeholder="comment here!"
+                        p={{ l: "1rem", r: "6rem" }}
+                        suffix={
+                            <Button
+                                pos="absolute"
+                                type="submit"
+                                bg="info700"
+                                hoverBg="info800"
+                                top="0"
+                                right="0"
+                                rounded={{ r: "md" }}
+                            >
+                                수정
+                            </Button>
+                        }
+                    />
+                </form>
+            </> : <>
+                <Div
+                    pos="absolute"
+                    top="1rem"
+                    right="1rem"
+                >
+                    {likes}
+                    <Icon onClick={handleClickLike} name={isLike ? "HeartSolid" : "Heart"} size="20px" color="info700" />
+                </Div>
+                <Div
+                    p={{ r: "4rem" }}
+                    style={{
+                        wordBreak: "break-all"
+                    }}
+                >
+                    {comment.content}
+                </Div>
+                {comment.is_modifiable ?
+                    <Button onClick={handleModifyBtn}>
+                        수정
+                    </Button> : <></>
+                }
+            </>}
         </Div>
     );
 }
--- frontend/src/dummy/DummyComment.js
+++ frontend/src/dummy/DummyComment.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Div, Icon, Button } from "atomize";
+import { Div, Icon, Input, Button } from "atomize";
 import "../css/Comment.css";
 
 function DummyComment({ comment, modifyRequest }) {
@@ -19,9 +19,25 @@ function DummyComment({ comment, modifyRequest }) {
     };
 
 
+    const [modifyMode, setMode] = useState(false);
+    const [content, setContent] = useState(comment.content);
+    const handleModifyBtn = (event) => {
+        event.preventDefault();
+        setContent(comment.content);
+        setMode(true);
+    };
+    const handleConentChange = (event) => {
+        setContent(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        modifyRequest(`api/comments/${comment.comment_id}`, { comment_id: comment.comment_id, content: content });
+        setMode(false);
+    };
+
     return (
-        <Div
-            className="comment-animation shadowDiv"
+        <Div className="comment-animation shadowDiv"
             pos="relative"
             m={{ y: "0.5rem" }}
             p="1rem"
@@ -29,29 +45,52 @@ function DummyComment({ comment, modifyRequest }) {
             minH="3rem"
             rounded="md"
         >
-            <Div
-                pos="absolute"
-                top="1rem"
-                right="1rem"
-            >
-                {likes}
-                <Icon onClick={handleClickLikeDummy} name={isLike ? "HeartSolid" : "Heart"} size="20px" color="info700" />
-            </Div>
-            <Div
-                p={{ r: "4rem" }}
-                style={{
-                    wordBreak: "break-all"
-                }}
-            >
-                {comment.content}
-            </Div>
-            <Button
-                pos="relative"
-                right="0"
-                onClick={()=>{modifyRequest("", {comment_id : comment.comment_id, content :"modify Test"})}}
-            >
-                수정
-            </Button>
+            {modifyMode ? <>
+                <form
+                    onSubmit={handleSubmit}
+                >
+                    <Input className="shadowDiv"
+                        type="text"
+                        value={content}
+                        onChange={handleConentChange}
+                        placeholder="comment here!"
+                        p={{ l: "1rem", r: "6rem" }}
+                        suffix={
+                            <Button
+                                pos="absolute"
+                                type="submit"
+                                bg="info700"
+                                hoverBg="info800"
+                                top="0"
+                                right="0"
+                                rounded={{ r: "md" }}
+                            >
+                                수정
+                            </Button>
+                        }
+                    />
+                </form>
+            </> : <>
+                <Div
+                    pos="absolute"
+                    top="1rem"
+                    right="1rem"
+                >
+                    {likes}
+                    <Icon onClick={handleClickLikeDummy} name={isLike ? "HeartSolid" : "Heart"} size="20px" color="info700" />
+                </Div>
+                <Div
+                    p={{ r: "4rem" }}
+                    style={{
+                        wordBreak: "break-all"
+                    }}
+                >
+                    {comment.content}
+                </Div>
+                <Button onClick={handleModifyBtn}>
+                    수정
+                </Button>
+            </>}
         </Div>
     );
 }
