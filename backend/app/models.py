ccdd21b1ff7cce4fe32aea2679aa7edb24c31795
jiyolla <jiyolla0919@gmail.com>
2021-09-14 20:27:05 +0900

    Modify nullablity of several column

--- backend/app/models.py
+++ backend/app/models.py
@@ -12,7 +12,7 @@ class Champion(Base):
     __tablename__ = 'champions'
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=False)
-    name = Column(String)
+    name = Column(String, nullable=False)
     icon_url = Column(String)
 
     skins = relationship('Skin', back_populates='champion')
@@ -22,18 +22,16 @@ class Skin(Base):
     __tablename__ = 'skins'
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=False)
-    name = Column(String)
+    name = Column(String, nullable=False)
     trimmed_image_url = Column(String)
     full_image_url = Column(String)
+    champion_id = Column(Integer, ForeignKey('champions.id'), nullable=False)
+    description = Column(String)
 
     # ----- To be deprecated -----
     price = Column(Integer, default=0)
     sale_price = Column(Integer, default=0)
     # ----- To be deprecated -----
-
-    champion_id = Column(Integer, ForeignKey('champions.id'))
-
-    description = Column(String, default='')
 
     champion = relationship('Champion', back_populates='skins')
     price_history = relationship('Price_History', back_populates='skin')
@@ -45,34 +43,34 @@ class Price_History(Base):
 
     skin_id = Column(Integer, ForeignKey('skins.id'), primary_key=True)
     date = Column(Date, primary_key=True)
-    price = Column(Integer, default=0)
-    sale_price = Column(Integer, default=0)
+    price = Column(Integer)
+    sale_price = Column(Integer)
     is_available = Column(Boolean, nullable=False, server_default='false')
     is_on_sale = Column(Boolean, nullable=False, server_default='false')
 
     skin = relationship('Skin', back_populates='price_history')
 
 
-class User(Base):
-    __tablename__ = 'users'
+# class User(Base):
+#     __tablename__ = 'users'
 
-    email_address = Column(String, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
+#     id = Column(Integer, primary_key=True)
+#     email_address = Column(String, unique=True, nullable=False)
 
-    comments = relationship('Comment', back_populates='author')
+#     comments = relationship('Comment', back_populates='author')
 
 
-class Comment(Base):
-    __tablename__ = 'comments'
+# class Comment(Base):
+#     __tablename__ = 'comments'
 
-    id = Column(Integer, primary_key=True)
-    skin_id = Column(Integer, ForeignKey('skins.id'), nullable=False)
-    author_username = Column(String, ForeignKey('users.username'), nullable=False)
-    content = Column(String, nullable=False)
-    created = Column(DateTime, default=datetime.today, nullable=False)
-    last_modified = Column(DateTime, default=datetime.today, nullable=False)
-    likes = Column(Integer, default=0, nullable=False)
-    dislikes = Column(Integer, default=0, nullable=False)
+#     id = Column(Integer, primary_key=True)
+#     skin_id = Column(Integer, ForeignKey('skins.id'), nullable=False)
+#     author_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+#     content = Column(String, nullable=False)
+#     created = Column(DateTime, default=datetime.today, nullable=False)
+#     last_modified = Column(DateTime, default=datetime.today, nullable=False)
+#     likes = Column(Integer, default=0, nullable=False)
+#     dislikes = Column(Integer, default=0, nullable=False)
 
-    skin = relationship('Skin', back_populates='comments')
-    author = relationship('User', back_populates='comments')
+#     skin = relationship('Skin', back_populates='comments')
+#     author = relationship('User', back_populates='comments')
