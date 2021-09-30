355e302672c499bcebab7bf04abd2ac02f15aa7e
jiyolla <jiyolla0919@gmail.com>
2021-09-30 14:29:51 +0900

    Fix static data update logic
    Previously, the script just wiped the champions and skins table, and
    rebuild the two tables with new data which was ok on development.
    
    Now the champions and skins table is actually 'updated' with new data.

--- backend/update_scripts/update_static.py
+++ backend/update_scripts/update_static.py
@@ -7,16 +7,32 @@ from app import models
 
 def update(db, version, champion):
     icon_url = f'http://ddragon.leagueoflegends.com/cdn/{version}/img/champion/{champion["id"]}.png'
-    db_champion = models.Champion(id=int(champion['key']), name=champion['name'], icon_url=icon_url)
-    db.add(db_champion)
-    champion_url = f'http://ddragon.leagueoflegends.com/cdn/{version}/data/ko_KR/champion/{champion["id"]}.json'
 
+    db_champion = db.query(models.Champion).filter(models.Champion.id == int(champion['key'])).first()
+    if db_champion is None:
+        db_champion = models.Champion(id=int(champion['key']), name=champion['name'], icon_url=icon_url)
+        db.add(db_champion)
+    else:
+        db_champion.id = int(champion['key'])
+        db_champion.name = champion['name']
+        db_champion.icon_url = icon_url
+
+    champion_url = f'http://ddragon.leagueoflegends.com/cdn/{version}/data/ko_KR/champion/{champion["id"]}.json'
     skins = requests.get(champion_url).json()['data'][champion['id']]['skins']
     for skin in skins:
         trimmed_image_url = f'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{champion["id"]}_{skin["num"]}.jpg'
         full_image_url = f'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/{champion["id"]}_{skin["num"]}.jpg'
-        db_skin = models.Skin(id=skin['id'], name=skin['name'], trimmed_image_url=trimmed_image_url, full_image_url=full_image_url, champion=db_champion)
-        db.add(db_skin)
+
+        db_skin = db.query(models.Skin).filter(models.Skin.id == skin['id']).first()
+        if db_skin is None:
+            db_skin = models.Skin(id=skin['id'], name=skin['name'], trimmed_image_url=trimmed_image_url, full_image_url=full_image_url, champion=db_champion)
+            db.add(db_skin)
+        else:
+            db_skin.id = skin['id']
+            db_skin.name = skin['name']
+            db_skin.trimmed_image_url = trimmed_image_url
+            db_skin.full_image_url = full_image_url
+            db_skin.champion = db_champion
     
 
 def main():
@@ -27,16 +43,10 @@ def main():
     champions = requests.get(champions_url).json()['data']
 
     print(f'Fetched {len(champions)} champions from version {version}.')
-    print('!!!CAUTION!!!')
-    print(f'Do you want to reconstruct {models.Champion.__tablename__!r} and {models.Skin.__tablename__!r} table?')
+    print(f'Do you want to update {models.Champion.__tablename__!r} and {models.Skin.__tablename__!r} table?')
     input('Press Enter to continue...')
 
     with SessionLocal() as db:
-        # ----- Replace these lines with SQL Update logic -----
-        db.query(models.Champion).delete()
-        db.query(models.Skin).delete()
-        # ----- Replace these lines with SQL Update logic -----
-
         for champion in tqdm(champions.values()):
             update(db, version, champion)
         db.commit()
